669472ef1da371c20c5ef914b4586a9b2d2013c5
BNedry <brice.theurillat@protonmail.ch>
2019-08-09 21:11:10 +0200

    Added list of search result

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -12,11 +12,11 @@ DOMPurify.setConfig({
 export default class SearchResult extends React.Component {
     render() {
         return (
-            <>
+            <div className="result">
                 <h3><a href={this.props.url} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(this.props.title)}}></a></h3>
                 <span className="result-source" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(this.props.source)}}></span>
                 <p className="result-desc" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(this.props.desc)}}></p>
-            </>
+            </div>
         );
     }
 }
