73af6d0d764d0de20b24df7886fbbae772e9c16a
BNedry <brice.theurillat@protonmail.ch>
2019-08-10 22:20:55 +0200

    Added results header

--- src/components/SearchResultList/SearchResultList.js
+++ src/components/SearchResultList/SearchResultList.js
@@ -4,12 +4,22 @@ import { observer } from "mobx-react";
 import SearchResult from "../SearchResult/SearchResult";
 
 import "./SearchResultList.css";
+import { AngleDownIcon } from "react-line-awesome";
 
 export default observer(class SearchResultList extends React.Component {
     render() {
         return (
             <div id="results">
-                {this.props.search.results.map((data) => <SearchResult className="result" {...data} key={data._id} />)}
+                <div id="results-header">
+                    <span id="results-title" class="uppercase">On the web</span>
+                    <div id="results-freshness">
+                        <span>Freshness</span>
+                        <AngleDownIcon />
+                    </div>
+                </div>
+                <div id="results-list">
+                    {this.props.search.results.map((data) => <SearchResult className="result" {...data} key={data._id} />)}
+                </div>
             </div>
         );
     }
