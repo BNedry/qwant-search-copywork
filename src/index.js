9f1545e6470d7675b159c210988966cab3be94f6
BNedry <brice.theurillat@protonmail.ch>
2019-08-10 14:32:46 +0200

    Added packages mobx and mobx-react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from "mobx";
+
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+configure({enforceActions: true});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
